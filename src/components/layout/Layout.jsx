1341a7da96adaf9afa449e2c36ef830a1d7cf6ef
DuongNB <duongnb>
2025-11-18 16:57:50 +0700

    .

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-red-300">
+    <div className="min-h-screen flex flex-col">
       {/* <Header /> */}
       <main className="flex-grow">{children}</main>
       {/* <Footer /> */}
